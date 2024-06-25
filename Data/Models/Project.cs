d34a735547f777fe4cbf9ea8066b132585f81499
Manh <130069804+duckmahn@users.noreply.github.com>
2024-06-25 21:24:36 +0700

    RenameControler

--- Data/Models/Project.cs
+++ Data/Models/Project.cs
@@ -5,4 +5,7 @@ public class Project
     public Guid Id { get; set; }//hidden
     public string? Title { get; set; }
     public ICollection<Todolist> Todolists { get; set; }
+
+    public ICollection<Users> UserId { get; set; }
+    public Guid AdminId { get; set; }
 }
