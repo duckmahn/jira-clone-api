7ee452863cd56dd3f367ed4d39a1e5d539294c1d
duckmahn <21dh113296@st.huflit.edu.vn>
2024-06-25 21:00:49 +0700

    add expire date

--- Data/DTO/TodoDTO.cs
+++ Data/DTO/TodoDTO.cs
@@ -6,5 +6,7 @@ public class TodoDTO
     public string? Description { get; set; }
     public string Status { get; set; }
     public Guid ProjectId { get; set; }
+
+    public DateTime ExpiredAt { get; set; }
 }
 
